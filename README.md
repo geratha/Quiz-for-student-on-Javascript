79d2cffa95de7f6ce31e60ac6cbbb30940d03e3e
Geratha <kandenzi018@gmail.com>
2018-12-21 18:05:33 +0200

    variable in js file

--- README.md
+++ README.md
@@ -7,3 +7,16 @@ Quiz of student on Javascript
 
 Uwamahoro Geratha
 
+## Description of project.
+
+This project is about javascript,how we can make questinaile for student lean javascript.
+
+##project setup instructions.
+
+bootstap,jquery
+
+##copyright and license information.
+
+(c) Uwamahoro Geratha,  MIT License 2018.
+
+# Quiz-for-student-on-Javascript
--- css/file.js
+++ css/file.js
@@ -1,37 +1,10 @@
 $(document).ready(function() {
-    $("#blanks form").submit(function(event){
-
-
-var q1=document.forms["myform"]["black"].value;
-var q2=document.forms["myform"]["black"].value;
-var q1=document.forms["myform"]["black"].value;
-var q2=document.forms["myform"]["black"].value;
-var q1=document.forms["myform"]["black"].value;
-var q2=document.forms["myform"]["black"].value;
-var marks=0;
-
-if(q1=="True")
-{
- marks=marks+2;
- }
-if(q2=="Tree")
-{
- marks=marks+3;
- }
-if(q3=="Var")
-{
- marks=marks+2;
-}
-if(q4=="Statically")
-{
- marks=marks+3;
-}
-if(q5=="False")
-{
- marks=marks+2;
-}
-if(q6=="No")
-{
- marks=marks+3;
-}
-alert("your score:"+marks);
+    $("#blanks form").submit(function(event){var q1=parseInt($("input:radio[name=answer1]:black").val());
+    var q2=parseInt($("input:radio[name=answer1]:black").val())
+    var q3=parseInt($("input:radio[name=answer1]:black").val())
+    var q4=parseInt($("input:radio[name=answer1]:black").val())
+    var q5=parseInt($("input:radio[name=answer1]:black").val())
+    var q6=parseInt($("input:radio[name=answer1]:blacxk").val())
+    var marks=0;
+    
+  
