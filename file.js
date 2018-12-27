bddb63af1933908328ecedc1ef492f0b92bc1c48
Perpetue <muperpety@gmail.com>
2018-12-27 15:48:30 +0200

    add all changes

---
+++ file.js
@@ -0,0 +1,35 @@
+
+// FRONT-END LOGIC
+
+$(document).ready(function() {
+    $('form').submit(function(event) {
+        event.preventDefault();
+
+        var answers = [];
+
+        $('.correct').each(function () {
+            answers.push($(this).prop('checked'));
+        })
+
+        console.log(answers);
+
+
+        $('#result').text("Your score is "+ getScore(answers));
+        document.getElementById("quiz").style.display="block";
+        $(".container").fadeOut();
+    })
+});
+
+// BUSINESS LOGIC
+
+function getScore(answers) {
+    var score = 2;
+
+    answers.forEach(function(answer) {
+        if (answer === true) {
+            score++;
+        }
+    });
+
+    return score;
+};
